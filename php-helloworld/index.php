aa23d353bc368209c42afb7d6250f95bdb113a31
Student User <student@workstation.lab.example.com>
2020-09-09 07:48:35 +0000

    updates

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,20 +1,13 @@
 <!DOCTYPE html>
 <html>
 <body>
-<<<<<<< HEAD
+
 <?php
-echo "Hello, World! OCP4";
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
 ?>
+
 </body>
 </html>
-=======
-
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?>
-
-</body>
-</html> 
->>>>>>> d0be2b555122e67dec6b8e10c577fe0e87b96a27
